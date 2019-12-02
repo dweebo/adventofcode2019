6847785f481c1ed64951553612ae6067d2cff513
Peter Hewitt <peter@beam.dental>
2019-12-02 09:49:32 -0500

    part 1 complete

--- lib/day2.rb
+++ lib/day2.rb
@@ -1,10 +1,25 @@
 class Day2
-  attr_reader :instructions
+  attr_accessor :instructions
 
   def initialize(file)
-    @instructions = File.read(file).split(",")
+    @instructions = File.read(file).split(",").map(&:to_i)
   end
 
   def execute
+    current_instruction = 0
+
+    while @instructions[current_instruction] != 99 do
+
+      if @instructions[current_instruction] == 1
+        @instructions[@instructions[current_instruction + 3]] =
+          @instructions[@instructions[current_instruction + 1]] +
+          @instructions[@instructions[current_instruction + 2]]
+      elsif @instructions[current_instruction] == 2
+        @instructions[@instructions[current_instruction + 3]] =
+          @instructions[@instructions[current_instruction + 1]] *
+          @instructions[@instructions[current_instruction + 2]]
+      end
+      current_instruction += 4
+    end
   end
 end
--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -16,5 +16,42 @@ RSpec.describe Day2 do
         expect(subject.instructions[0]).to eq(2)
       end
     end
+
+    context "sample two" do
+      let(:file) {
+        "lib/day2-test2.txt"
+      }
+
+      it "reads instructions" do
+        subject.execute
+        expect(subject.instructions[3]).to eq(6)
+      end
+    end
+
+    context "sample three" do
+      let(:file) {
+        "lib/day2-test3.txt"
+      }
+
+      it "reads instructions" do
+        subject.execute
+        expect(subject.instructions).to eq([
+          30,1,1,4,2,5,6,0,99
+        ])
+      end
+    end
+
+    context "part1" do
+      let(:file) {
+        "lib/day2.txt"
+      }
+      it "completes part 1" do
+        subject.instructions[1] = 12
+        subject.instructions[2] = 2
+        subject.execute
+
+        expect(subject.instructions[0]).to eq(0)
+      end
+    end
   end
 end
