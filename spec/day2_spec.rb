6d3073e9289e5d66f47659c3e60d689cb39e6363
Peter Hewitt <peter@beam.dental>
2019-12-02 10:01:36 -0500

    day 2 part 2 complete

--- spec/day2_spec.rb
+++ spec/day2_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe Day2 do
     context "sample one" do
       let(:file) { "lib/day2-test1.txt" }
 
-      it "reads instructions" do
+      it "executes add" do
         subject.execute
         expect(subject.instructions[0]).to eq(2)
       end
@@ -18,7 +18,7 @@ RSpec.describe Day2 do
     context "sample two" do
       let(:file) { "lib/day2-test2.txt" }
 
-      it "reads instructions" do
+      it "executes mult" do
         subject.execute
         expect(subject.instructions[3]).to eq(6)
       end
@@ -27,7 +27,7 @@ RSpec.describe Day2 do
     context "sample three" do
       let(:file) { "lib/day2-test3.txt" }
 
-      it "reads instructions" do
+      it "executes both" do
         subject.execute
         expect(subject.instructions).to eq([
           30,1,1,4,2,5,6,0,99
@@ -43,6 +43,37 @@ RSpec.describe Day2 do
       it "completes part 1" do
         subject.execute
         expect(subject.instructions[0]).to eq(3765464)
+      end
+    end
+
+    context "part2-brute" do
+      let(:file) { "lib/day2.txt" }
+      let(:expected_answer) { 19690720 }
+      it "brute forces the answer" do
+
+        for noun in (0..99) do
+          for verb in (0..99) do
+            day2 = Day2.new(file, noun, verb)
+            day2.execute
+            if day2.instructions[0] == expected_answer
+              puts "noun=#{noun} verb=#{verb}"
+            end
+          end
+        end
+      end
+    end
+
+    context "part2-verify" do
+      let(:file) { "lib/day2.txt" }
+      let(:noun) { 76 }
+      let(:verb) { 10 }
+      let(:expected_answer) { 19690720 }
+      let(:solution) { 7610 }
+
+      it "completes part 2" do
+        subject.execute
+        expect(subject.instructions[0]).to eq(expected_answer)
+        expect(solution).to eq(100 * noun + verb)
       end
     end
   end
